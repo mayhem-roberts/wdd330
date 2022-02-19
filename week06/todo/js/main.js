1c29b4ef7d8646fef76160a5381b5197e94a4d8e
Meagan Roberts <reaganmoberts@gmail.com>
2022-02-19 13:38:56 -0700

    commit

--- week06/todo/js/main.js
+++ week06/todo/js/main.js
@@ -1,12 +1,56 @@
-const form = document.querySelector(".add-task");
-const entry = document.getElementById("entry");
+//import Todo from "./todo";
+
+let toDoList = [];
+
+class Todo {
+    constructor(elementId) {
+        this.parentElement = document.getElementById(elementId);
+    }
+
+    addTask = () => {
+        // get input from entry box
+        let entryText = document.getElementById('entry').value;
+
+        // create a new task: id, content, and completion status
+        const newTask = {
+            id: new Date(),
+            content: entryText,
+            completed: false
+        };
 
 
+        toDoList.push(newTask);
+        localStorage.setItem("tasks", JSON.stringify(toDoList));
+    }
 
-function addTask() {
-    let task = entry.value;
-    console.log(task);
+    displayTask = () => {
+        let toDo = localStorage.getItem("tasks")
+        toDoList = JSON.parse(toDo);
+
+        //console.log(toDoList[1].content);
+
+        toDoList.forEach(index => {
+            console.log(index.content);
+            let li = document.createElement("li");
+
+            li.textContent = index.content;
+            document.getElementById("tasks").appendChild(li);
+        });
+        
+
+    }
+
 }
 
+const toDos = new Todo("tasks")
+const submitButton = document.getElementById("submitButton");
+
+window.addEventListener("load", () => {
+    toDos.displayTask();
+});
 // submit form
-form.addEventListener("submit", addTask);
+submitButton.addEventListener("click", (event) => {
+    console.log("submit")
+    event.preventDefault();
+    toDos.addTask();
+});
